b5fc6a960306a47281e0d60962acdb1220b5968b
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-05-08 11:50:14 -0700

    Add method: return id(name) with highest score

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -24,5 +24,19 @@ const dateRange = (req, res) => {
   });
 }
 
+// Method: Retrieve highest scored unique id (returns an name)
+const idHighScore = (req, res) => {
+  // include name and score for easier data visualizing
+  const queryString = 'SELECT name, score FROM documents WHERE score = (SELECT Max(score) FROM documents);';
+  db.query(queryString, [req.params.id], (err, data) => {
+    if (err) {
+      throw new Error(err);
+    }
+    data = data.map(obj => obj.name);
+    res.send(data[0]);
+  })
+}
+
 exports.scoresById = scoresById;
 exports.dateRange = dateRange;
+exports.idHighScore = idHighScore;
--- src/router.js
+++ src/router.js
@@ -4,5 +4,8 @@ const controllers = require('./controllers');
 
 router.get('/getScores/:id', controllers.scoresById);
 router.get('/dateRange/:startDate/:endDate', controllers.dateRange);
+// Method: Retrieve highest scored unique id
+router.get('/highScore/:id', controllers.idHighScore);
+
 
 module.exports = router;
